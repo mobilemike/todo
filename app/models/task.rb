34bc8a6f269da23da67414d892367fd6ac554f1f
Your Name <mobilemike@gmail.com>
2012-08-23 12:08:51 -0400

    Add days as scopes on the Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,19 @@
 class Task < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :assigned_date
   
   validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: {:scope => :assigned_date}
+
+  scope :yesterday, ->{ where(:assigned_date => Date.yesterday)}
+  scope :today, ->{ where(:assigned_date => Date.today)}
+  scope :tomorrow, ->{ where(:assigned_date => Date.tomorrow)}
+
+
+  after_initialize :init
+
+	private
+  def init
+    self.assigned_date ||= Date.today
+  end
+
 end
