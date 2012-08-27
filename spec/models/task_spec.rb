4464817fa179da0a881d2428363dbb9d250043e3
Your Name <mobilemike@gmail.com>
2012-08-27 17:09:25 -0400

    Add FactoryGirl syntax helper mixin

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,36 +4,36 @@ TODAY = Date.current
 
 describe Task do
 	it "has a valid factory" do
-    FactoryGirl.create(:task).should be_valid
+    create(:task).should be_valid
 	end
 
 	it "is invalid without a title" do
-	  FactoryGirl.build(:task, title: nil).should_not be_valid
+	  build(:task, title: nil).should_not be_valid
 	end
 
 	context "using scopes" do
 	  before :each do
-	  	@yesterday = FactoryGirl.create(:task,
-						  												title: "Yesterday's Task",
-																			assigned_date: (TODAY - 1))
-	  	@today = FactoryGirl.create(:task,
-																	title: "Today's Task",
-																	assigned_date: TODAY)
-	  	@tomorrow = FactoryGirl.create(:task,
-																		 title: "Tomorrow's Task",
-																		 assigned_date: (TODAY + 1))
+	  	@yesterday = create(:task,
+						  						title: "Yesterday's Task",
+													assigned_date: (TODAY - 1))
+	  	@today = create(:task,
+											title: "Today's Task",
+											assigned_date: TODAY)
+	  	@tomorrow = create(:task,
+												 title: "Tomorrow's Task",
+												 assigned_date: (TODAY + 1))
 	  end
 
 		it "is invalid with a dupilcate title on the same day" do
-			FactoryGirl.build(:task,
-												 title: "Today's Task",
-												 assigned_date: TODAY).should_not be_valid
+			build(:task,
+			     	title: "Today's Task",
+						assigned_date: TODAY).should_not be_valid
 		end
 
 		it "is valid with a duplicate title on different days" do
-			FactoryGirl.build(:task,
-												 title: "Today's Task",
-												 assigned_date: TODAY + 1.day).should be_valid
+			build(:task,
+						title: "Today's Task",
+						assigned_date: TODAY + 1.day).should be_valid
 		end
       
 		it "returns only yesterdays tasks when asked" do
